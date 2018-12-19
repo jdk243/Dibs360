a06545d2fab1cc9f6dc139a56c091214e629d055
Andrew Scagnelli-Townley <andrew.scagnelli@1stdibs.com>
2018-12-19 16:03:47 -0500

    tweaking moving

--- dibs3603d.js
+++ dibs3603d.js
@@ -9,9 +9,9 @@ import Entity from 'Entity';
 import { spaces } from './spaces';
 
 const baseLocation = {
-    x: 0,
+    x: -3,
     y: -1,
-    z: -2,
+    z: 3.5,
 };
 
 export class Dibs3603D extends React.Component {
@@ -35,14 +35,25 @@ export class Dibs3603D extends React.Component {
     }
 
     handleNewLocation(event) {
-        const nextLocation = get(spaces, get(event, 'detail.location'));
-        const currentLocation = get(spaces, get(event, 'detail.currentLocation'));
-        console.log('nextLocation', nextLocation);
-        console.log('currentLocation', currentLocation);
+        const nextLocationLinks = (get(spaces, get(event, 'detail.location')) || {}).links || [];
+        const currentLocationName = get(event, 'detail.currentLocation');
+        const nextLink = nextLocationLinks.find(link => {
+            return get(link, 'link') === currentLocationName;
+        });
+        const translation = get(nextLink, 'translation');
+        console.log('translation', translation);
+        if (translation) {
+            this.setState({
+                x: baseLocation.x + (translation[0].translateX / 4),
+                y: baseLocation.y,
+                z: baseLocation.z + (translation[2].translateZ / 4),
+            });
+        }
     }
 
     render() {
         const { x, y, z } = this.state;
+        console.log(`x: ${x}, y: ${y}, z: ${z}`);
         return (
             <View style={{
               transform: [{ translate: [x, y, z] }]
